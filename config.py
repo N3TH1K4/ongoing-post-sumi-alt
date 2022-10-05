e0cf2f70671e5fcd3f992497851db356d7271835
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-10-05 18:27:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 API_ID = 18983092
 API_HASH = 'a6005a70e88369b4fb08b8350ebbdd35'
-BOT_TOKEN = '5436027078:AAFzMja2jgglu0LEMFhw-d3pwPCUWnxB7CI'
+BOT_TOKEN = '5555472213:AAFAeMOBc8OVrs_l16Is4FCs0xHv2ZmcBzA'
 MONGO_URL = "mongodb+srv://TROJ3N:Nethika123@cluster0.uppg6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
